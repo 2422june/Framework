08ba3a542948f64bc1ffc0ca9e1322c576b57196
2422june <ck66661254@gmail.com>
2023-10-24 11:00:18 +0900

    edit Data Manager and add info

--- Framework/Assets/Scripts/Manager/DataManager.cs
+++ Framework/Assets/Scripts/Manager/DataManager.cs
@@ -2,7 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DataManager : ManagerBase
+[System.Serializable]
+public class Card
 {
+    public int id;
+    public string name;
+    public string imageUrl;
+}
 
+[System.Serializable]
+public class Data
+{
+    public List<Card> cards = new List<Card>();
+}
+
+public class DataManager : MonoBehaviour
+{
+    private void Start()
+    {
+        Load();
+    }
+
+    private void Load()
+    {
+        TextAsset asset = Resources.Load<TextAsset>("Data");
+        Data cardDatas  = JsonUtility.FromJson<Data>(asset.text);
+    }
 }
